8382a8e03fd2e0842071dfcc20ab3e21c353df79
chirag1992m <chirag1992m.cc@gmail.com>
2013-03-17 03:51:37 +0530

    A simple solution for question arrays/find_the_missing_number

---
+++ arrays/find the missing number-method1.cpp
@@ -0,0 +1,31 @@
+/* method 1 */
+/* Here I'll sort the array using turbo sort as the numbers 
+are specifically in between 1 to n-1 */
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char const *argv[]) {
+	int n, x;
+	/* get the value of n */
+	cin>>n;
+
+	/* make an array of size n of type boolean */
+	bool *arr = new bool[n];
+	memset(arr, 0, n);
+
+	for(int i=0; i<n-1; i++) {
+		cin>>x;
+
+		/* set the value found to be true in the array */
+		arr[x] = true;
+	}
+
+	for(int i=0; i<n; i++) {
+		if(!arr[i]) {
+			cout<<i<<endl;
+			break;
+		}
+	}
+
+	return 0;
+}
