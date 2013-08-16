8d3e9a1e1f0ad5e9492c8475859ae512ca282473
chirag1992m <chirag1992m.cc@gmail.com>
2013-08-16 15:46:41 +0530

    Started with cormen questions. Cormen chapter 2 question added. geeks for geeks question on multiplicity of 3 added

---
+++ cormen/chapter_2/calculating_inversions.cpp
@@ -0,0 +1,92 @@
+/*
+	@author: Chirag Maheshwari
+
+	This program calculates the number of inversions in an array.
+	Let A[1...n] be an array of n distinct numbers. If i < j and A[i] > A[j], then tha pair(i, j) is called an inversion.
+*/
+
+/* Method 1
+	This is the brute force method to calculate the inversions.*/
+#include <iostream>
+
+using namespace std;
+
+int countInversion_method1(int arr[], int size) {
+	int inversionCount = 0;
+
+	for(int i=0; i<size; i++) {
+		for(int j=i+1; j<size; j++) {
+			if(arr[i] > arr[j])
+				inversionCount++;
+		}
+	}
+
+	return inversionCount;
+}
+
+int countInversion_method2(int arr[], int start, int end) {
+	/* Base value case */
+	if((end - start) < 2)
+		return 0;
+
+	int midPoint = ((end-start)/2) + start;
+	int countFromfirstPart = 0, countFromSecondPart = 0, totalCount = 0;
+
+	/* Dividing the array in two parts and recursively counting the inverions on every part */
+	countFromfirstPart = countInversion_method2(arr, start, midPoint);
+	countFromSecondPart = countInversion_method2(arr, midPoint, end);
+
+	/* Merging the two arrays here */
+		/* First copy the parts of array in a temporary array */
+	int sizeL = (midPoint - start),
+		sizeR = (end - midPoint),
+		arrL[sizeL],
+		arrR[sizeR],
+		size = end - start;
+
+	for(int i=0; i<sizeL; i++) {
+		arrL[i] = arr[start + i];
+	}
+	for(int i=0; i<sizeR; i++) {
+		arrR[i] = arr[midPoint + i];
+	}
+
+	int l=0, r=0;
+	for(int i=start; i<start + size; i++) {
+		if(l < sizeL && r < sizeR) {
+			if(arrL[l] <= arrR[r]) {
+				arr[i] = arrL[l];
+				l++;
+			} else {
+				arr[i] = arrR[r];
+				r++;
+				totalCount = totalCount + (sizeL - l);
+			}
+		} else if(l < sizeL) {
+			arr[i] = arrL[l];
+			l++;
+		} else {
+			arr[i] = arrR[r];
+			r++;
+		}
+	}
+
+	totalCount = totalCount + countFromfirstPart + countFromSecondPart;
+
+	return totalCount;
+}
+
+int main() {
+	int size, arr[100000];
+
+	cout<<"Enter the number of elements in array: ";
+	cin>>size;
+	cout<<"Enter the elements now: "<<endl;
+
+	for(int i=0; i<size; i++) {
+		cin>>arr[i];
+	}
+
+	cout<<"From brute force method: "<<countInversion_method1(arr, size)<<endl;
+	cout<<"From divide-and-conquer paradigm (extending merge sort): "<<countInversion_method2(arr, 0, size)<<endl;
+}
