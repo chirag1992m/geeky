c5399472245a641e897a5a964db2c9c0cea2dd61
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-03-05 00:26:40 -0500

    Minimum Depth Binary Search Tree, Made the solution more concise based on editorials

--- LeetCode/minimum-depth-binary-tree.cpp
+++ LeetCode/minimum-depth-binary-tree.cpp
@@ -16,29 +16,15 @@ The minimum depth is the number of nodes along the shortest path from the root n
  */
 class Solution {
 public:
-    int minimum(int x, int y) {
-        return (x > y) ? y : x;
-    }
-    
-    int maximum(int x, int y) {
-        return (x > y) ? x : y;
-    }
-    
-    int minDepthRecursive(TreeNode* root, int currentDepth = 0) {
-        if(root == NULL)
-            return currentDepth;
-            
-        if (root->left == NULL || root->right == NULL) {
-            //If there is only one subtree (or no subtree)
-            //Then only that subtree will be considered in the depth
-            //This of BST with all children in the left side. Depth != 0 in that case.
-            return maximum(minDepthRecursive(root->left, currentDepth+1), minDepthRecursive(root->right, currentDepth+1));
-        } else {
-            return minimum(minDepthRecursive(root->left, currentDepth+1), minDepthRecursive(root->right, currentDepth+1));
-        }
-    }
     
     int minDepth(TreeNode* root) {
-        return minDepthRecursive(root);
+        if(root == NULL)
+            return 0;
+            
+        if (root->left == NULL || root->right == NULL) {
+            return 1+max(minDepth(root->left), minDepth(root->right));
+        } else {
+            return 1+min(minDepth(root->left), minDepth(root->right));
+        }
     }
 };
